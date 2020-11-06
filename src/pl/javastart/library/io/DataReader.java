5d60341daf02cb7c9431efe03462531e3abed8f2
luki25591 <lukasz.wnek@onet.eu>
2020-11-06 14:04:43 +0100

    Utworzenie klasy DataReader; Utworzenie Scannera oraz przeniesienie tworzenie książek;

---
+++ src/pl/javastart/library/io/DataReader.java
@@ -0,0 +1,31 @@
+package pl.javastart.library.io;
+
+import pl.javastart.library.model.Book;
+
+import java.util.Scanner;
+
+public class DataReader {
+    private Scanner sc = new Scanner(System.in);
+
+    public void close(){
+        sc.close();
+    }
+
+    public Book readAndCerateBook(){
+        System.out.println("Tytuł:");
+        String title = sc.nextLine();
+        System.out.println("Autor:");
+        String authir = sc.nextLine();
+        System.out.println("Wydawnictwo:");
+        String publisher = sc.nextLine();
+        System.out.println("ISBN:");
+        String ISBN = sc.nextLine();
+        System.out.println("Rok wydania:");
+        int releaseDate = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Liczba stron:");
+        int pages = sc.nextInt();
+        sc.nextLine();
+        return new Book(title, authir, releaseDate,pages, publisher, ISBN);
+    }
+}
